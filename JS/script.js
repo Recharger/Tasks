f577ab1eb045bd68b1ae861506de22cc0b374c74
Recharger <evgeniipyrlog@mail.ru>
2016-09-21 22:31:29 +0300

    SecondStepContributor_v2

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,46 @@
 (function () {
     $(document).ready(function () {
-            getContributorsList();
-        }
-    );
+        getContributorsList();
+    });
 
 
     function getContributorsList() {
         var promise = $.get("https://api.github.com/repos/thomasdavis/backbonetutorials/contributors");
         $.when(promise).done(function (data) {
-            console.log(data);
-            window.contibutors = data;
-            $(".list").attr("size", data.length);
             separateContributorsList(data);
+            $(".contributor-type").change(function () {
+                showContributorsFromType($(this).val());
+            })
         })
     }
 
     function separateContributorsList(object) {
         object.forEach(function (element) {
+            setGroupStatus(element);
             createContributorElement(element);
         })
 
     }
 
     function createContributorElement(person) {
-        $(".container").append('<div class="contributor-item"><img src="' + person.avatar_url + '" class="img-circle contributor">' +
+        $(".contributors-" + person.group).append('<div class="contributor-item ' + person.group + '"><img src="' + person.avatar_url + '" class="img-circle contributor">' +
             '<h3>' + person.login + '</h3><div/>');
+    }
+
+    function setGroupStatus(person) {
+        if(person.contributions > 50) person.group = 'gold';
+        else if(person.contributions > 5) person.group = 'silver';
+        else person.group = 'bronze';
+    }
+
+    function showContributorsFromType(group) {
+        if(group === "all") {
+            $(".contributors-gold, .contributors-silver, .contributors-bronze").show();
+        } else {
+            $(".contributors-gold").hide();
+            $(".contributors-silver").hide();
+            $(".contributors-bronze").hide();
+            $(".contributors-" + group).show();
+        }
     }
 })();
