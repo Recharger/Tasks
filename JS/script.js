467d277cafd9b679ae35e191899162a2d51e7a1b
Recharger <evgeniipyrlog@mail.ru>
2016-09-21 18:33:55 +0300

    FirstStepContributor_v2

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,29 @@
-$(document).ready(function () {
-        getContributorsList();
+(function () {
+    $(document).ready(function () {
+            getContributorsList();
+        }
+    );
+
+
+    function getContributorsList() {
+        var promise = $.get("https://api.github.com/repos/thomasdavis/backbonetutorials/contributors");
+        $.when(promise).done(function (data) {
+            console.log(data);
+            window.contibutors = data;
+            $(".list").attr("size", data.length);
+            separateContributorsList(data);
+        })
     }
-);
 
+    function separateContributorsList(object) {
+        object.forEach(function (element) {
+            createContributorElement(element);
+        })
 
-function getContributorsList() {
-    var promise = $.get("https://api.github.com/repos/thomasdavis/backbonetutorials/contributors");
-    $.when(promise).done(function () {
-        $(".list").attr("size", promise.responseJSON.length);
-        separateContributorsList(promise.responseJSON)
-    })
-}
+    }
 
-function separateContributorsList(object) {
-    object.forEach(function (element) {
-        createContributorElement(element);
-    })
-
-}
-
-function createContributorElement(person) {
-    $(".list").append('<option>' + person.login + '</option>');
-}
+    function createContributorElement(person) {
+        $(".container").append('<div class="contributor-item"><img src="' + person.avatar_url + '" class="img-circle contributor">' +
+            '<h3>' + person.login + '</h3><div/>');
+    }
+})();
