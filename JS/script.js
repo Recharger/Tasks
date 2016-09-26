cfb959f0db01934afc516098e8f490fff19b5980
Recharger <evgeniipyrlog@mail.ru>
2016-09-26 18:11:21 +0300

    add SASS features, add 404 solution

--- JS/script.js
+++ JS/script.js
@@ -28,11 +28,11 @@
     function getContributorsList() {
         var promise = $.get("https://api.github.com/repos/thomasdavis/backbonetutorials/contributors");
         $.when(promise).done(function (data) {
-            sortContributorsArray(data);
+            sortContributorsArray(data,$(".sort-selector").val());
             separateContributorsList(data);
             $(".sort-selector").change(function () {
                 $(".contributors").html("");
-                sortContributorsArray(data);
+                sortContributorsArray(data,$(".sort-selector").val());
                 separateContributorsList(data);
                 showContributorsFromType($(".contributor-type").val());
 
@@ -40,13 +40,19 @@
             $(".contributor-type").change(function () {
                 showContributorsFromType($(this).val());
             })
+        }).fail(function (data) {
+            new PNotify({
+                type: 'error',
+                title: 'OOPS!',
+                text: data.statusText
+            });
         });
     }
 
     function separateContributorsList(object) {
-        object.forEach(function (element) {
-            setGroupStatus(element);
-            createContributorElement(element);
+        object.forEach(function (userInformation) {
+            setGroupStatus(userInformation);
+            createContributorElement(userInformation);
         })
 
     }
@@ -57,9 +63,15 @@
     }
 
     function setGroupStatus(person) {
-        if(person.contributions > GroupLimits.gold.minValue) person.group = GroupLimits.gold.name;
-        else if(person.contributions > GroupLimits.silver.minValue) person.group = GroupLimits.silver.name;
-        else person.group = GroupLimits.bronze.name;
+        if(person.contributions > GroupLimits.gold.minValue) {
+            person.group = GroupLimits.gold.name;
+        }
+        else if(person.contributions > GroupLimits.silver.minValue) {
+            person.group = GroupLimits.silver.name;
+        }
+        else {
+            person.group = GroupLimits.bronze.name;
+        }
     }
 
     function showContributorsFromType(group) {
@@ -71,45 +83,49 @@
         }
     }
 
-    function sortContributorsArray(arr) {
-        var sortType = $(".sort-selector").val();
+    function sortContributorsArray(arr,sortType) {
         if(sortType == "asc") {
             arr.sort(function (a, b) {
                 if (a.login.toLowerCase() > b.login.toLowerCase()) {
                     return 1;
-                } else {
-                    return -1;
                 }
+                return -1;
             });
         }
         if(sortType == "desc") {
             arr.sort(function (a, b) {
                 if (a.login.toLowerCase() < b.login.toLowerCase()) {
                     return 1;
-                } else {
-                    return -1;
                 }
+                return -1;
             });
         }
     }
 
-    function parseUserData(parameters) {
-        var login = parameters.login;
+    function parseUserData(options) {
+        var login = options.login;
         var promise = $.get("https://api.github.com/users/" + login);
         $.when(promise).done(function (data) {
             $("#myModal").modal("show");
+
             fillModalWindowFields(data);
+        }).fail(function (data) {
+            new PNotify({
+                type: 'error',
+                title: 'OOPS!',
+                text: data.statusText
+            });
         });
 
     }
 
     function fillModalWindowFields(user) {
         $(".modal-title").html("Information about: " + user.login);
-        var name = (user.name)? '<p>Name: ' + capitalizeFirstLetter(user.name) + '</p>' : '';
-        var company = (user.company)? '<p>Company: ' + capitalizeFirstLetter(user.company) + '</p>' : '';
-        var blog = (user.blog)? '<p>Blog: <a href="'+ user.blog +'" target="_blank">'+ user.blog +'</a></p>' : '';
-        var email = (user.email)? '<p>Email: <a href="mailto:' + user.email + '">' + user.email + '</a></p>' : '';
-        var location = (user.location)? '<p>Location: ' + user.location + "</p>" : '';
+        var name = (user.name) ? '<p>Name: ' + capitalizeFirstLetter(user.name) + '</p>' : '';
+        var company = (user.company) ? '<p>Company: ' + capitalizeFirstLetter(user.company) + '</p>' : '';
+        var blog = (user.blog) ? '<p>Blog: <a href="'+ user.blog +'" target="_blank">'+ user.blog +'</a></p>' : '';
+        var email = (user.email) ? '<p>Email: <a href="mailto:' + user.email + '">' + user.email + '</a></p>' : '';
+        var location = (user.location) ? '<p>Location: ' + user.location + "</p>" : '';
 
         $(".modal-body").html('<img src="' + user.avatar_url + '"> ' +
             ((name || company || blog || email || location)?
