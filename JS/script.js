4a9b6bb34bf3b569e46d7a52d70faa593f7a703b
Recharger <evgeniipyrlog@mail.ru>
2016-09-29 13:46:25 +0300

    add last amendment, refactor ternary operator

--- JS/script.js
+++ JS/script.js
@@ -17,26 +17,25 @@
     $(document).ready(function () {
         getContributorsList();
         var cache = {};
-        $(".contributors").on("click",".contributor-item",function () {
+        $(".contributors").on("click", ".contributor-item", function () {
             parseUserData({
                 login: $(this).find("h3").html(),
                 downloaded: cache
             });
         });
-        $(".modal").on("hidden.bs.modal",function () {
-            $(".modal-body").html('');
-            $(".modal-title").html('');
+        $(".modal").on("hidden.bs.modal", function () {
+            $(".modal-body, .modal-title").empty();
         });
     });
 
     function getContributorsList() {
         var promise = $.get("https://api.github.com/repos/thomasdavis/backbonetutorials/contributors");
         $.when(promise).done(function (data) {
-            sortContributorsArray(data,$(".sort-selector").val());
+            sortContributorsArray(data);
             separateContributorsList(data);
             $(".sort-selector").change(function () {
-                $(".contributors").html("");
-                sortContributorsArray(data,$(".sort-selector").val());
+                $(".contributors").empty();
+                data.reverse();
                 separateContributorsList(data);
                 showContributorsFromType($(".contributor-type").val());
 
@@ -53,8 +52,8 @@
         });
     }
 
-    function separateContributorsList(object) {
-        object.forEach(function (userInformation) {
+    function separateContributorsList(arrContributors) {
+        arrContributors.forEach(function (userInformation) {
             setGroupStatus(userInformation);
             createContributorElement(userInformation);
         })
@@ -67,10 +66,10 @@
     }
 
     function setGroupStatus(person) {
-        if(person.contributions > GroupLimits.gold.minValue) {
+        if (person.contributions > GroupLimits.gold.minValue) {
             person.group = GroupLimits.gold.name;
         }
-        else if(person.contributions > GroupLimits.silver.minValue) {
+        else if (person.contributions > GroupLimits.silver.minValue) {
             person.group = GroupLimits.silver.name;
         }
         else {
@@ -79,7 +78,7 @@
     }
 
     function showContributorsFromType(group) {
-        if(group === "all") {
+        if (group === "all") {
             $(".contributors").children().show();
         } else {
             $(".contributors").children().hide();
@@ -87,31 +86,21 @@
         }
     }
 
-    function sortContributorsArray(arr,sortType) {
-        if(sortType == "asc") {
+    function sortContributorsArray(arr) {
             arr.sort(function (a, b) {
                 if (a.login.toLowerCase() > b.login.toLowerCase()) {
                     return 1;
                 }
                 return -1;
             });
-        }
-        if(sortType == "desc") {
-            arr.sort(function (a, b) {
-                if (a.login.toLowerCase() < b.login.toLowerCase()) {
-                    return 1;
-                }
-                return -1;
-            });
-        }
     }
 
     function parseUserData(options) {
         var login = options.login;
-        if(!options.downloaded[login]){
+        if (!options.downloaded[login]){
             var promise = $.get("https://api.github.com/users/" + login);
             $.when(promise).done(function (data) {
-                $("#myModal").modal("show");
+                $(".modal").modal("show");
                 options.downloaded[login] = data;
                 fillModalWindowFields(data);
             }).fail(function (data) {
@@ -122,26 +111,27 @@
                 });
             });
         } else {
-            $("#myModal").modal("show");
+            $(".modal").modal("show");
             fillModalWindowFields(options.downloaded[login]);
         }
+    }
 
-
-
+    function generateModalFilling(user) {
+        console.log(user);
+        var filling = (user.name != undefined) ? '<p>Name: ' + capitalizeFirstLetter(user.name) + '</p>' : '';
+        filling += (user.company != undefined) ? '<p>Company: ' + capitalizeFirstLetter(user.company) + '</p>' : '';
+        filling += (user.blog != undefined) ? '<p>Blog: <a href="'+ user.blog +'" target="_blank">'+ user.blog +'</a></p>' : '';
+        filling += (user.email != undefined) ? '<p>Email: <a href="mailto:' + user.email + '">' + user.email + '</a></p>' : '';
+        filling += (user.location != undefined) ? '<p>Location: ' + user.location + "</p>" : '';
+        var image = '<img src="' + user.avatar_url + '"> ';
+        var failString = 'There is no information about this user';
+        if (filling === '') return image + failString;
+        return image + filling;
     }
 
     function fillModalWindowFields(user) {
         $(".modal-title").html("Information about: " + user.login);
-        var name = (user.name) ? '<p>Name: ' + capitalizeFirstLetter(user.name) + '</p>' : '';
-        var company = (user.company) ? '<p>Company: ' + capitalizeFirstLetter(user.company) + '</p>' : '';
-        var blog = (user.blog) ? '<p>Blog: <a href="'+ user.blog +'" target="_blank">'+ user.blog +'</a></p>' : '';
-        var email = (user.email) ? '<p>Email: <a href="mailto:' + user.email + '">' + user.email + '</a></p>' : '';
-        var location = (user.location) ? '<p>Location: ' + user.location + "</p>" : '';
-
-        $(".modal-body").html('<img src="' + user.avatar_url + '"> ' +
-            ((name || company || blog || email || location)?
-                name + company + blog + email + location
-                    : '<h2>There is no information about this user</h2>'));
+        $(".modal-body").html(generateModalFilling(user));
     }
 
     function capitalizeFirstLetter(string) {
